705442a5bb5f681cab0155beea16013ca5818bee
wilson <wilson.lin@shell.fans>
2025-07-08 17:27:15 +0800

    feat: update c16

--- book/src/chapter/c16.rs
+++ book/src/chapter/c16.rs
@@ -1,18 +1,50 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 pub fn c16_1_main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+}
+
+pub fn c16_2_main() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
 
-    handle.join().unwrap();
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {received}");
+    }
 }
--- book/src/main.rs
+++ book/src/main.rs
@@ -1,5 +1,5 @@
-use book::chapter::c16::c16_1_main;
+use book::chapter::c16::{c16_1_main, c16_2_main};
 
 fn main() {
-    c16_1_main()
+    c16_2_main()
 }
