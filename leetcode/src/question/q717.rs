4bb8edccbe1308bda1d54529891c6c9793270fbe
Wilson <wwilson008@gmail.com>
2025-11-18 17:03:08 +0800

    update q717.rs

---
+++ leetcode/src/question/q717.rs
@@ -0,0 +1,12 @@
+pub fn is_one_bit_character(bits: Vec<i32>) -> bool {
+    let n = bits.len();
+    let mut i = 0;
+    while i < n-1{
+        if bits[i] == 0{
+            i += 1;
+        }else{
+            i += 2;
+        }
+    }
+    i == n-1
+}
