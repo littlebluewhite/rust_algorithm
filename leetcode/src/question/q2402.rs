42390d83d20cced5a6df01b32d708529b155e5c8
Wilson <wwilson008@gmail.com>
2025-12-28 03:56:25 +0800

    update q2402.rs

--- leetcode/src/question/q2402.rs
+++ leetcode/src/question/q2402.rs
@@ -3,45 +3,41 @@ use std::collections::BinaryHeap;
 
 pub fn most_booked(n: i32, meetings: Vec<Vec<i32>>) -> i32 {
     let n = n as usize;
-    let mut meetings: Vec<(i64, i64)> = meetings
+    let mut meetings = meetings
         .into_iter()
         .map(|m| (m[0] as i64, m[1] as i64))
-        .collect();
+        .collect::<Vec<(i64, i64)>>();
     meetings.sort_by_key(|m| m.0);
-
     let mut available: BinaryHeap<Reverse<usize>> = BinaryHeap::new();
-    for room in 0..n {
-        available.push(Reverse(room));
-    }
     let mut busy: BinaryHeap<Reverse<(i64, usize)>> = BinaryHeap::new();
     let mut count = vec![0usize; n];
-
+    for i in 0..n {
+        available.push(Reverse(i));
+    }
     for (start, end) in meetings {
-        while let Some(&Reverse((t, room))) = busy.peek() {
-            if t <= start {
-                busy.pop();
+        while let Some(&Reverse((busy_end, room))) = busy.peek(){
+            if busy_end<=start{
                 available.push(Reverse(room));
-            } else {
+                busy.pop();
+            }else{
                 break;
             }
         }
-
-        if let Some(Reverse(room)) = available.pop() {
-            count[room] += 1;
+        if let Some(Reverse(room)) = available.pop(){
+            count[room]+=1;
             busy.push(Reverse((end, room)));
-        } else {
-            let Reverse((t, room)) = busy.pop().unwrap();
-            let duration = end - start;
-            let new_end = t + duration;
-            count[room] += 1;
-            busy.push(Reverse((new_end, room)));
+        }else{
+            if let Some(Reverse((busy_end, room))) = busy.pop() {
+                let delta = busy_end-start;
+                count[room]+=1;
+                busy.push(Reverse((end+delta, room)));
+            }
         }
     }
-
     let mut best = 0usize;
-    for room in 1..n {
-        if count[room] > count[best] {
-            best = room;
+    for room in 1..n{
+        if count[room]>count[best]{
+            best = room
         }
     }
     best as i32
