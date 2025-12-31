f90d975f8e0102d63f2ca67ab84a2d274baeba2a
Wilson <wwilson008@gmail.com>
2025-12-31 18:08:39 +0800

    update c17.rs

--- book/src/chapter/c17.rs
+++ book/src/chapter/c17.rs
@@ -206,3 +206,30 @@ pub fn c17_3_3_main() {
         );
     });
 }
+
+pub fn c17_3_4_main() {
+    trpl::run(async {
+        let slow = async {
+            trpl::sleep(Duration::from_millis(100)).await;
+            "I finished!"
+        };
+
+        match timeout(slow, Duration::from_millis(200)).await {
+            Ok(message) => println!("Succeeded with '{message}'"),
+            Err(duration) => {
+                println!("Failed after {} seconds", duration.as_secs())
+            }
+        }
+    });
+
+}
+
+async fn timeout<F: Future>(
+    future_to_try: F,
+    max_time: Duration,
+) -> Result<F::Output, Duration> {
+    match trpl::race(future_to_try, trpl::sleep(max_time)).await {
+        Either::Left(output) => Ok(output),
+        Either::Right(_) => Err(max_time),
+    }
+}
