fc031019f8d7e2578b6c47cc8e1c97fccb595ee9
wilson <wilson.lin@shell.fans>
2026-01-09 02:09:57 +0800

    update aq1339.rs

--- leetcode/src/question/aq1339.rs
+++ leetcode/src/question/aq1339.rs
@@ -20,39 +20,38 @@ use std::cell::RefCell;
 use std::rc::Rc;
 const MOD: i128 = 1e9 as i128 + 7;
 pub fn max_product(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-    fn sum(node: &Option<Rc<RefCell<TreeNode>>>) -> i64{
-        if let Some(rc) = node{
-            let (val, left, right) = {
-                let n = rc.borrow();
-                (n.val as i64, n.left.clone(), n.right.clone())
-            };
-            val + sum(&left) + sum(&right)
-        }else{
-            0
-        }
-    }
-
-    fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, total: i64, best: &mut i128) -> i64{
-        if let Some(rc) = node{
-            let (val, left, right) = {
-                let n = rc.borrow();
-                (n.val, n.left.clone(), n.right.clone())
-            };
-            let left_sum = dfs(&left, total, best);
-            let right_sum = dfs(&right, total, best);
-            let sub_sum = left_sum + right_sum + val as i64;
-            let product = sub_sum as i128 * ((total - sub_sum) as i128);
-            if product > *best{
-                *best = product;
-            }
-            sub_sum
-        }else{
-            0
-        }
-    }
-
     let total = sum(&root);
-    let mut best = 0i128;
-    dfs(&root, total, &mut best);
-    (best % MOD) as i32
+    let mut max = 0i128;
+    dfs(&root, total, &mut max);
+    (max % MOD) as i32
+}
+
+fn sum(node: &Option<Rc<RefCell<TreeNode>>>) -> i64{
+    if let Some(rc) = node{
+        let (val, left, right) = {
+            let tree_node = rc.borrow();
+            (tree_node.val, tree_node.left.clone(), tree_node.right.clone())
+        };
+        val as i64 + sum(&left) + sum(&right)
+    }else{
+        0
+    }
+}
+
+fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, total: i64, max: &mut i128) -> i64{
+    if let Some(rc) = node{
+        let (val, left, right) ={
+            let tree_node = rc.borrow();
+            (tree_node.val, tree_node.left.clone(), tree_node.right.clone())
+        };
+        let left = dfs(&left, total, max);
+        let right = dfs(&right, total, max);
+        let product = val as i64 + left + right;
+        if product as i128 * (total-product) as i128 > *max{
+            *max = product as i128;
+        }
+        product
+    }else{
+        0
+    }
 }
