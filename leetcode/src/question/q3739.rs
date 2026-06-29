3d0d9f897013da4c3bccafbc35e06260d91dd8a2
Wilson <wwilson008@gmail.com>
2026-06-30 04:33:24 +0800

    update q3739.rs

--- leetcode/src/question/q3739.rs
+++ leetcode/src/question/q3739.rs
@@ -39,3 +39,26 @@ pub fn count_majority_subarrays(nums: Vec<i32>, target: i32) -> i64 {
     }
     ans
 }
+
+pub fn count_majority_subarrays2(nums: Vec<i32>, target: i32) -> i64 {
+    let n = nums.len();
+    let size = 2 * n +1;
+    let to_idx = |v: i64| -> usize {n+v as usize+1};
+    let mut cnt = vec![0i64; size+1];
+    let mut cur = 0i64;
+    let mut ans = 0i64;
+    let mut less = 0i64;
+    cnt[to_idx(0)] = 1;
+    for &num in &nums{
+        if num==target{
+            cur += 1;
+            less += cnt[to_idx(cur-1)];
+        }else{
+            cur -= 1;
+            less -= cnt[to_idx(cur)];
+        }
+        cnt[to_idx(cur)] += 1;
+        ans += less;
+    }
+    ans
+}
