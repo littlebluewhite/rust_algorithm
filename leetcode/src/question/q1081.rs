127bea0cf74d34b45199868e4a5082f2352ce899
Wilson <wwilson008@gmail.com>
2026-07-20 16:46:52 +0800

    update q1081.rs

--- leetcode/src/question/q1081.rs
+++ leetcode/src/question/q1081.rs
@@ -1,3 +1,28 @@
 pub fn smallest_subsequence(s: String) -> String {
-    s
+    let b = s.as_bytes();
+    let n = b.len();
+    let mut last = vec![0;26];
+    for (i, &c) in b.iter().enumerate() {
+        last[(c - b'a') as usize] = i;
+    }
+    let mut in_stack = vec![false;26];
+    let mut stack: Vec<u8> = Vec::with_capacity(n);
+    for (i, &c) in b.iter().enumerate() {
+        let idx = (c-b'a') as usize;
+        if in_stack[idx] {
+            continue;
+        }
+        while let Some(&top) = stack.last() {
+            let t = (top - b'a') as usize;
+            if t > idx && last[t] > i{
+                stack.pop();
+                in_stack[t] = false;
+            } else {
+                break;
+            }
+        }
+        stack.push(c);
+        in_stack[idx] = true;
+    }
+    String::from_utf8(stack).unwrap()
 }
