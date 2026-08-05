8a103ff780c22a3919191f2adeb31de92d5dbd58
Wilson <wwilson008@gmail.com>
2026-08-05 12:26:45 +0800

    update q1406.rs

--- leetcode/src/question/q1406.rs
+++ leetcode/src/question/q1406.rs
@@ -1,20 +1,33 @@
 pub fn stone_game_iii(stone_value: Vec<i32>) -> String {
+    let mut dp1 = 0;
+    let mut dp2 = 0;
+    let mut dp3 = 0;
     let n = stone_value.len();
-    let mut dp = vec![vec![0; n]; n + 1];
-    for i in 0..n {
-        dp[1][i] = stone_value[i];
-    }
-    for len in 2..=n {
-        for l in 0..=n - len {
-            let r = l + len - 1;
-            let take_left = stone_value[l] - dp[len - 1][l + 1];
-            let take_right = stone_value[r] - dp[len - 1][l];
-            dp[len][l] = take_left.max(take_right);
+    for i in (0..n).rev() {
+        let mut value = 0;
+        let mut best = i32::MIN;
+        for take in 1..=3{
+            if i + take > n {
+                break;
+            }
+            value += stone_value[i + take - 1];
+            let diff = match take {
+                1 => dp1,
+                2 => dp2,
+                3 => dp3,
+                _ => unreachable!(),
+            };
+            best = best.max(value - diff);
         }
+        dp3 = dp2;
+        dp2 = dp1;
+        dp1 = best;
     }
-    match dp[n][0] {
-        0 => "Tie".to_string(),
-        _ if dp[n][0] > 0 => "Alice".to_string(),
-        _ => "Bob".to_string(),
+    if dp1 > 0{
+        "Alice".to_string()
+    } else if dp1 < 0 {
+        "Bob".to_string()
+    } else {
+        "Tie".to_string()
     }
 }
